8f8e5c3134e8fb7782e9433aafa5efd1ae7f397a
joshualyguessennd <josue@dinari.com>
2026-07-30 14:15:15 +0200

    add a buyback share to the liquidation fee split

--- contracts/position/DecreasePositionCollateralUtils.sol
+++ contracts/position/DecreasePositionCollateralUtils.sol
@@ -660,88 +660,21 @@ library DecreasePositionCollateralUtils {
         return (values, getEmptyFees(fees));
     }
 
+    // @dev zeroed fees for an early return. Only the funding values and the collateral price
+    //      carry over: the funding values may still be needed to update a partially closed
+    //      position, everything else is deliberately dropped.
     function getEmptyFees(
         PositionPricingUtils.PositionFees memory fees
     ) internal pure returns (PositionPricingUtils.PositionFees memory) {
-        PositionPricingUtils.PositionReferralFees memory referral = PositionPricingUtils.PositionReferralFees({
-            referralCode: bytes32(0),
-            affiliate: address(0),
-            trader: address(0),
-            totalRebateFactor: 0,
-            affiliateRewardFactor: 0,
-            adjustedAffiliateRewardFactor: 0,
-            traderDiscountFactor: 0,
-            totalRebateAmount: 0,
-            traderDiscountAmount: 0,
-            affiliateRewardAmount: 0
-        });
+        PositionPricingUtils.PositionFees memory _fees;
 
-        PositionPricingUtils.PositionProFees memory pro = PositionPricingUtils.PositionProFees({
-            traderTier: 0,
-            traderDiscountFactor: 0,
-            traderDiscountAmount: 0
-        });
+        _fees.funding.claimableLongTokenAmount = fees.funding.claimableLongTokenAmount;
+        _fees.funding.claimableShortTokenAmount = fees.funding.claimableShortTokenAmount;
+        _fees.funding.latestFundingFeeAmountPerSize = fees.funding.latestFundingFeeAmountPerSize;
+        _fees.funding.latestLongTokenClaimableFundingAmountPerSize = fees.funding.latestLongTokenClaimableFundingAmountPerSize;
+        _fees.funding.latestShortTokenClaimableFundingAmountPerSize = fees.funding.latestShortTokenClaimableFundingAmountPerSize;
 
-        // allow the accumulated funding fees to still be claimable
-        // return the latestFundingFeeAmountPerSize, latestLongTokenClaimableFundingAmountPerSize,
-        // latestShortTokenClaimableFundingAmountPerSize values as these may be used to update the
-        // position's values if the position will be partially closed
-        PositionPricingUtils.PositionFundingFees memory funding = PositionPricingUtils.PositionFundingFees({
-            fundingFeeAmount: 0,
-            claimableLongTokenAmount: fees.funding.claimableLongTokenAmount,
-            claimableShortTokenAmount: fees.funding.claimableShortTokenAmount,
-            latestFundingFeeAmountPerSize: fees.funding.latestFundingFeeAmountPerSize,
-            latestLongTokenClaimableFundingAmountPerSize: fees.funding.latestLongTokenClaimableFundingAmountPerSize,
-            latestShortTokenClaimableFundingAmountPerSize: fees.funding.latestShortTokenClaimableFundingAmountPerSize
-        });
-
-        PositionPricingUtils.PositionBorrowingFees memory borrowing = PositionPricingUtils.PositionBorrowingFees({
-            borrowingFeeUsd: 0,
-            borrowingFeeAmount: 0
-        });
-
-        PositionPricingUtils.PositionUiFees memory ui = PositionPricingUtils.PositionUiFees({
-            uiFeeReceiver: address(0),
-            uiFeeReceiverFactor: 0,
-            uiFeeAmount: 0
-        });
-
-        PositionPricingUtils.PositionLiquidationFees memory liquidation = PositionPricingUtils.PositionLiquidationFees({
-            liquidationFeeUsd: 0,
-            liquidationFeeAmount: 0,
-            liquidationFeeValidatorFactor: 0,
-            liquidationFeeAmountForValidator: 0,
-            liquidationFeeInsuranceFactor: 0,
-            liquidationFeeAmountForInsurance: 0
-        });
-
-        // all fees are zeroed even though funding may have been paid
-        // the funding fee amount value may not be accurate in the events due to this
-        PositionPricingUtils.PositionFees memory _fees = PositionPricingUtils.PositionFees({
-            referral: referral,
-            pro: pro,
-            funding: funding,
-            borrowing: borrowing,
-            ui: ui,
-            liquidation: liquidation,
-            collateralTokenPrice: fees.collateralTokenPrice,
-            positionFeeFactor: 0,
-            protocolFeeAmount: 0,
-            positionFeeVeAlphaFactor: 0,
-            positionFeeTreasuryFactor: 0,
-            positionFeeBuybackFactor: 0,
-            veAlphaFeeAmount: 0,
-            treasuryFeeAmount: 0,
-            buybackFeeAmount: 0,
-            validatorFeeAmount: 0,
-            insuranceFeeAmount: 0,
-            feeAmountForPool: 0,
-            positionFeeAmountForPool: 0,
-            positionFeeAmount: 0,
-            totalCostAmountExcludingFunding: 0,
-            totalCostAmount: 0,
-            totalDiscountAmount: 0
-        });
+        _fees.collateralTokenPrice = fees.collateralTokenPrice;
 
         return _fees;
     }
